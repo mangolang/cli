f8868f9fac6f882c27dc4d2d5ba0482ccdeb3d3e
Mark <mverleg.noreply@gmail.com>
2020-07-22 23:17:29 +0200

    More details about compile targets

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,4 @@ mod options;
 #[paw::main]
 fn main(args: options::Mango) {
     eprintln!("Warning: all Mango CLI options are subject to change");
-
 }
--- src/options/compile.rs
+++ src/options/compile.rs
@@ -9,10 +9,23 @@ pub struct CompileCmd {
 // subsubcommand!
 #[derive(StructOpt)]
 pub enum Target {
+    #[structopt(about = "Mango intermediary representation (for debugging)")]
     IR {
-        applications: u32
+        #[structopt(
+            long = "json",
+            help = "Dump as pretty-printed json for easy human inspection"
+        )]
+        json: bool,
+        #[structopt(
+            long = "packed",
+            help = "Dump as efficiently-packed binary format, for small size and fast parsing"
+        )]
+        packed: bool,
     },
+    #[structopt(about = "WebAssembly files and bindings")]
     WASM {},
+    #[structopt(about = "Self-contained executable")]
     Executable {},
+    #[structopt(about = "Docker image that runs your application")]
     Docker {},
 }
