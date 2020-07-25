c00e7f97e638eb66671bc793445af951c40b1843
Mark <mverleg.noreply@gmail.com>
2020-07-25 13:05:33 +0200

    Improve argument help

--- src/options/mod.rs
+++ src/options/mod.rs
@@ -3,16 +3,25 @@ use ::structopt::StructOpt;
 pub mod compile;
 
 #[derive(StructOpt)]
-#[structopt(about = "Warning: all Mango CLI options are subject to change!")]
+#[structopt(
+    //author = "Mango programming language CLI",
+    after_help = "Mango documentation: https://docs.mangocode.org/\nWarning: all Mango CLI options are subject to change!"
+)]
 pub struct MangoArgs {
-    #[structopt(short = "v", long, help = "Show verbose information for debugging.")]
+    #[structopt(
+        short = "v",
+        long,
+        help = "Show verbose information for debugging.",
+        hidden_short_help=true
+    )]
     pub verbose: bool,
 
     #[structopt(
         conflicts_with = "verbose",
         short = "q",
         long = "quiet",
-        help = "Only show the most important output."
+        help = "Only show the most important output.",
+        hidden_short_help=true
     )]
     pub quiet: bool,
 
