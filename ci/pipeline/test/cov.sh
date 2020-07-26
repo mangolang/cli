1a0678ab4a2f90e3ed5fc2fb75fedf47aa8f77c5
Mark <mverleg.noreply@gmail.com>
2020-07-26 16:21:14 +0200

    Code coverage finally works

--- ci/pipeline/test/cov.sh
+++ ci/pipeline/test/cov.sh
@@ -2,13 +2,11 @@
 
 source "${BASH_SOURCE%/*}/../shared.sh"
 
-# Compute code coverage. This needs a separate set of flags, so there is a dedicated script.
-
-#TODO @mark: TEMPORARY! REMOVE THIS!
-#mkdir -p "$RELEASE_PATH/coverage"
-#docker run --rm --mount type=bind,src="$RELEASE_PATH/coverage",dst='/coverage' 'mango_ci:latest' \
-#    bash 'cargo_for_coverage.sh' run
-CHECK_NIGHTLY /bin/bash -s <<EOF
-cargo --offline test --all-targets --all-features
-
-EOF
+CHECK_NIGHTLY bash -c "\
+cargo --offline test --all-targets --all-features --quiet;\
+grcov target/debug/ -s . -t html --llvm --branch -o ./target/debug/coverage/;\
+cp target/debug/coverage/index.html /release/coverage.html"
+if [ ! -f "$RELEASE_PATH/coverage.html" ]; then
+    printf 'failed to generate coverage\n' 1>&2
+    exit 1
+fi
