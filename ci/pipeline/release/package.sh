be71579a70b10720a601a969e48874b9376abecf
Mark <mverleg.noreply@gmail.com>
2020-08-01 01:07:39 +0200

    Debug release packaging step

--- ci/pipeline/release/package.sh
+++ ci/pipeline/release/package.sh
@@ -4,9 +4,9 @@ source "${BASH_SOURCE%/*}/../shared.sh"
 
 CHECK_NIGHTLY rm -f "/release/"*".zip"
 # shellcheck disable=SC2010
-if ! ls -1qA . | grep -q . ; then
+if ! ls -1qA "${RELEASE_PATH}" | grep -q . ; then
     printf 'did not find any files to release\n'  1>&2
     exit 1
 fi
-CHECK_NIGHTLY bash -c "cd /release; zip -r '${RELEASE_NAME}.zip' ."
+CHECK_NIGHTLY bash -c "cd /release; ls -als; zip -r '${RELEASE_NAME}.zip' ."
 cp "/${RELEASE_PATH}/${RELEASE_NAME}.zip" "./${RELEASE_NAME}.zip"
