0bf3ddbf01113db395c2f65d21a18c57a3af0ed2
Mark <mverleg.noreply@gmail.com>
2021-04-11 22:22:16 +0200

    Add run and test commands

---
+++ src/options/exec_test.rs
@@ -0,0 +1,10 @@
+use ::structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub struct TestCmd {
+    #[structopt(
+        long = "pattern",
+        help = "Regular expression pattern for test names."
+    )]
+    pub pattern: Option<String>,
+}
