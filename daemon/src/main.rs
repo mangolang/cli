51716aea7cba41eec2106f629381eecff1ff523b
Mark <mverleg.noreply@gmail.com>
2021-04-14 21:49:30 +0200

    Start listening for websocket connections

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,8 +1,18 @@
+use ::ws::listen;
+
 use crate::options::MangodArgs;
 
 mod options;
 
 #[paw::main]
 fn main(args: MangodArgs) {
-    println!("welcome to mango daemon");
+    assert!(!args.host.contains(":"));
+    assert!(!args.host.contains(" "));
+    let addr = format!("{}:{}", &args.host, &args.port);
+    println!("starting mangod, listening on {}", &addr);
+    listen(&addr, |out| {
+        move |msg| {
+            out.send(msg)
+        }
+    }).unwrap()
 }
