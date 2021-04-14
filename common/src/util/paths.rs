1192e065cc42586c43c0c4599578fb704ceff4cd
Mark <mverleg.noreply@gmail.com>
2021-04-14 22:31:51 +0200

    Unit test for reading and writing lock file

--- common/src/util/paths.rs
+++ common/src/util/paths.rs
@@ -1,17 +1,25 @@
 use ::std::env::temp_dir;
-use ::std::fs;
+use ::std::fs::create_dir_all;
 use ::std::fs::File;
 use ::std::io::BufReader;
 use ::std::path::PathBuf;
+use std::io::BufWriter;
 
 use ::dirs::cache_dir;
 use ::dirs::home_dir;
 use ::serde::{Deserialize, Serialize};
-use std::io::BufWriter;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct LockInfo {
     pid: Option<u32>,
+}
+
+impl LockInfo {
+    pub fn new(pid: Option<u32>) -> Self {
+        LockInfo {
+            pid
+        }
+    }
 }
 
 fn get_cache_dir() -> PathBuf {
@@ -19,7 +27,7 @@ fn get_cache_dir() -> PathBuf {
         .or_else(|| home_dir())
         .unwrap_or_else(|| temp_dir());
     pth.push("mango");
-    fs::create_dir_all(&pth)
+    create_dir_all(&pth)
         .expect("could not create mango cache directory");
     pth
 }
@@ -33,15 +41,29 @@ fn get_lock_file() -> PathBuf {
 pub fn load_lock() -> LockInfo {
     let pth = get_lock_file();
     let reader = BufReader::new(File::open(&pth)
-        .unwrap_or_else(|_| panic!("could not access the mangod lock file: '{}'", pth.to_string_lossy())));
+        .unwrap_or_else(|err| panic!("could not access the mangod lock file: '{}', reason: {}", pth.to_string_lossy(), err)));
     serde_json::from_reader(reader)
-        .unwrap_or_else(|_| panic!("could not read the content of the mangod lock file (is it valid json?): '{}'", pth.to_string_lossy()))
+        .unwrap_or_else(|err| panic!("could not read the content of the mangod lock file (is it valid json?): '{}', reason: {}", pth.to_string_lossy(), err))
 }
 
 pub fn store_lock(info: &LockInfo) {
     let pth = get_lock_file();
     let writer = BufWriter::new(File::open(&pth)
-        .unwrap_or_else(|_| panic!("could not access the mangod lock file: '{}'", pth.to_string_lossy())));
+        .unwrap_or_else(|err| panic!("could not access the mangod lock file: '{}', reason: {}", pth.to_string_lossy(), err)));
     serde_json::to_writer_pretty(writer, info)
-        .unwrap_or_else(|_| panic!("could not write to the mangod lock file: '{}'", pth.to_string_lossy()))
+        .unwrap_or_else(|err| panic!("could not write to the mangod lock file: '{}', reason: {}", pth.to_string_lossy(), err))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_write_pid() {
+        let before = LockInfo::new(Some(1234));
+        store_lock(&before);
+        assert!(get_lock_file().is_file());
+        let after = load_lock();
+        assert_eq!(before, after);
+    }
 }
