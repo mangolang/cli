573eda0839c20cb26d7cc093f13aae6f1dfe6b00
Mark <mverleg.noreply@gmail.com>
2021-04-14 22:39:12 +0200

    Fix race condition in the lock file test

--- common/src/util/paths.rs
+++ common/src/util/paths.rs
@@ -2,8 +2,10 @@ use ::std::env::temp_dir;
 use ::std::fs::create_dir_all;
 use ::std::fs::File;
 use ::std::io::BufReader;
+use ::std::io::BufWriter;
 use ::std::path::PathBuf;
-use std::io::BufWriter;
+use ::std::time::SystemTime;
+use ::std::time::UNIX_EPOCH;
 
 use ::dirs::cache_dir;
 use ::dirs::home_dir;
@@ -12,12 +14,18 @@ use ::serde::{Deserialize, Serialize};
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct LockInfo {
     pid: Option<u32>,
+    timestamp: u64,
 }
 
 impl LockInfo {
     pub fn new(pid: Option<u32>) -> Self {
+        let since_the_epoch_s = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
         LockInfo {
-            pid
+            pid,
+            timestamp: since_the_epoch_s,
         }
     }
 }
@@ -48,7 +56,7 @@ pub fn load_lock() -> LockInfo {
 
 pub fn store_lock(info: &LockInfo) {
     let pth = get_lock_file();
-    let writer = BufWriter::new(File::open(&pth)
+    let writer = BufWriter::new(File::create(&pth)
         .unwrap_or_else(|err| panic!("could not access the mangod lock file: '{}', reason: {}", pth.to_string_lossy(), err)));
     serde_json::to_writer_pretty(writer, info)
         .unwrap_or_else(|err| panic!("could not write to the mangod lock file: '{}', reason: {}", pth.to_string_lossy(), err))
@@ -56,11 +64,24 @@ pub fn store_lock(info: &LockInfo) {
 
 #[cfg(test)]
 mod tests {
+    use ::serial_test::serial;
+
     use super::*;
 
+    #[serial]
     #[test]
     fn read_write_pid() {
         let before = LockInfo::new(Some(1234));
+        store_lock(&before);
+        assert!(get_lock_file().is_file());
+        let after = load_lock();
+        assert_eq!(before, after);
+    }
+
+    #[serial]
+    #[test]
+    fn read_write_no_pid() {
+        let before = LockInfo::new(None);
         store_lock(&before);
         assert!(get_lock_file().is_file());
         let after = load_lock();
