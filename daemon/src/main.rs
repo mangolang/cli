467c0a22718345ee99a3a77e8e76d3c961f3ff94
Mark <mverleg.noreply@gmail.com>
2021-04-14 21:45:59 +0200

    Make mangod options work

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,5 +1,8 @@
+use crate::options::MangodArgs;
+
 mod options;
 
-pub fn main() {
+#[paw::main]
+fn main(args: MangodArgs) {
     println!("welcome to mango daemon");
 }
--- daemon/src/options.rs
+++ daemon/src/options.rs
@@ -1,5 +1,7 @@
 use ::structopt::StructOpt;
 
+//TODO @mark: make multiple commands, of which 'start' is the implicit one
+
 #[derive(StructOpt, Debug)]
 #[structopt(
     after_help = "Mango documentation: https://docs.mangocode.org/\nWarning: all Mango daemon options are subject to change!"
@@ -7,18 +9,17 @@ use ::structopt::StructOpt;
 #[rustfmt::skip]
 pub struct MangodArgs {
     #[structopt(
-        short = "h",
+        short = "b",
         long = "hostname",
-        default = "localhost",
-        help = "Hostname to listen on.",
+        default_value = "localhost",
+        help = "Hostname to listen on. Do not expose mangod to untrusted networks.",
     )]
     pub host: String,
 
     #[structopt(
         short = "p",
         long = "port",
-        //TODO @mark:
-        default = "47557",
+        default_value = "47558",
         help = "Port to listen on.",
     )]
     pub port: u16,
