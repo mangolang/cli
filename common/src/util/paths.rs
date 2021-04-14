d0a4a978d21f587bfed36426b9c79620b999606d
Mark <mverleg.noreply@gmail.com>
2021-04-14 22:27:32 +0200

    Write the mangod lock file

--- common/src/util/paths.rs
+++ common/src/util/paths.rs
@@ -7,6 +7,7 @@ use ::std::path::PathBuf;
 use ::dirs::cache_dir;
 use ::dirs::home_dir;
 use ::serde::{Deserialize, Serialize};
+use std::io::BufWriter;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LockInfo {
@@ -32,12 +33,15 @@ fn get_lock_file() -> PathBuf {
 pub fn load_lock() -> LockInfo {
     let pth = get_lock_file();
     let reader = BufReader::new(File::open(&pth)
-        .unwrap_or_else(|_| panic!("could not create the mangod lock file: '{}'", pth.to_string_lossy())));
+        .unwrap_or_else(|_| panic!("could not access the mangod lock file: '{}'", pth.to_string_lossy())));
     serde_json::from_reader(reader)
         .unwrap_or_else(|_| panic!("could not read the content of the mangod lock file (is it valid json?): '{}'", pth.to_string_lossy()))
 }
 
 pub fn store_lock(info: &LockInfo) {
     let pth = get_lock_file();
-    unimplemented!()
+    let writer = BufWriter::new(File::open(&pth)
+        .unwrap_or_else(|_| panic!("could not access the mangod lock file: '{}'", pth.to_string_lossy())));
+    serde_json::to_writer_pretty(writer, info)
+        .unwrap_or_else(|_| panic!("could not write to the mangod lock file: '{}'", pth.to_string_lossy()))
 }
