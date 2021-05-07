ec93dd964369c08904b307fa8154b12b086e5d54
Mark <mverleg.noreply@gmail.com>
2021-05-08 00:00:44 +0200

    Restructure cli e2e tests

--- src/cli/e2e.rs
+++ src/cli/e2e.rs
@@ -12,18 +12,16 @@ use ::std::time::Duration;
 use ::lazy_static::lazy_static;
 use ::serde_json::Map;
 use ::serde_json::Value;
-use ::structopt::clap::ErrorKind;
-use ::structopt::StructOpt;
 use ::tempfile::TempDir;
 
-use crate::cli;
-use crate::cli::options::MangoArgs;
+use serde::__private::from_utf8_lossy;
+use std::path::Path;
 
 lazy_static! {
     static ref MANGO_EXE: RwLock<Option<PathBuf>> = RwLock::new(None);
 }
 
-fn run_daemon(args: &[&str], test: impl FnOnce(Output)) {
+fn compile_exe() {
     MANGO_EXE.write().unwrap().get_or_insert_with(|| {
         println!("starting building executable");
         let result = Command::new("cargo").arg("build").arg("--message-format=json").output().unwrap();
@@ -49,63 +47,76 @@ fn run_daemon(args: &[&str], test: impl FnOnce(Output)) {
         assert!(exes.len() > 0, "could not find executable in cargo output");
         exes.into_iter().next().unwrap()
     });
+}
 
-    let mut cmd = vec![];
-    cmd.push(MANGO_EXE.read().unwrap().unwrap().to_string_lossy().into_owned());
-    cmd.push("run-as-daemon".to_owned());
-    for arg in args {
-        cmd.push((*arg).to_owned());
-    }
-    let out = Command::new(MANGO_EXE.read().unwrap().unwrap())
-        .arg("run-as-daemon")
+fn run_cli(exe_path: &Path, args: &[&str]) -> Output {
+    //TODO @mark: does this cause double locking?
+    let exe_read = MANGO_EXE.read().unwrap();
+    let dir = TempDir::new().unwrap();
+    env::set_var("MANGO_USER_CACHE_PATH", &dir.path().to_string_lossy().into_owned());
+    let output = Command::new(exe_path)
         .args(args)
-        .spawn()
+        .output()
         .unwrap();
-    test(out);
-    child.join();
-    todo!("keep locked until here to make tests single-threaded")
+    // Explicitly drop to make sure the lock is held until here.
+    drop(exe_read);
+    drop(dir);
+    output
+}
+
+fn run_with_daemon(args: &[&str], test: impl FnOnce(Output)) {
+    compile_exe();
+
+    let dir = TempDir::new().unwrap();
+    env::set_var("MANGO_USER_CACHE_PATH", &dir.path().to_string_lossy().into_owned());
+    MANGO_EXE.read().unwrap().map(|exe_pth| {
+        let mut child = Command::new(&exe_pth)
+            .arg("run-as-daemon")
+            .spawn()
+            .unwrap();
+        test(run_cli(&exe_pth, args));
+        child.wait();
+    });
+    drop(dir);
 }
 
 #[test]
 fn show_help() {
-    run_daemon(&["-h"]);
-    assert_eq!(
-        ErrorKind::HelpDisplayed,
-        MangoArgs::from_iter_safe(&["mango", "-h"]).unwrap_err().kind
-    );
-    assert_eq!(
-        ErrorKind::HelpDisplayed,
-        MangoArgs::from_iter_safe(&["mango", "--help"]).unwrap_err().kind
+    run_with_daemon(
+        &["-h"],
+        |output| {
+            let out = from_utf8_lossy(&output.stdout);
+            assert!(out.contains("mango"));
+        },
     );
 }
 
 #[test]
 fn compile_ir() {
-    let dir = TempDir::new().unwrap();
-    env::set_var("MANGO_USER_CACHE_PATH", &dir.path().to_string_lossy().into_owned());
-    run_daemon();
-    let args = MangoArgs::from_iter_safe(&["mango", "compile"]).unwrap();
-    cli(args).unwrap()
+    run_with_daemon(
+        &["compile"],
+        |output| assert!(output.status.success()),
+    );
 }
 
 #[test]
 fn daemon_start_stop() {
     let dir = TempDir::new().unwrap();
     env::set_var("MANGO_USER_CACHE_PATH", &dir.path().to_string_lossy().into_owned());
-    run_daemon();
+    compile_exe();
 
     // Start
-    let res = do_cli(&["daemon", "start", "-p", "47559"]);
+    let res = run_cli(&["daemon", "start", "-p", "47559"]);
     let start_txt = from_utf8(&res.stderr).unwrap();
     println!("starting:\n{}/starting", start_txt);
     assert!(res.status.success(), "{}", start_txt);
 
-    let res = do_cli(&["daemon", "get", "status"]);
+    let res = run_cli(&["daemon", "get", "status"]);
     let out = from_utf8(&res.stdout).unwrap().trim();
     assert_eq!(out, "running");
 
     // Stop
-    let res = do_cli(&["daemon", "stop"]);
+    let res = run_cli(&["daemon", "stop"]);
     let stop_txt = from_utf8(&res.stderr).unwrap();
     println!("stopping:\n{}/stopped", stop_txt);
     assert!(res.status.success(), "{}", from_utf8(&res.stderr).unwrap());
@@ -114,7 +125,7 @@ fn daemon_start_stop() {
     //TODO get rid of sleep when the server no longer sleeps
     sleep(Duration::from_millis(75));
 
-    let res = do_cli(&["daemon", "get", "status"]);
+    let res = run_cli(&["daemon", "get", "status"]);
     let out = from_utf8(&res.stdout).unwrap().trim();
     assert_eq!(out, "not-started");
 }
