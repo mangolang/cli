97ca02d3cd0dd10637c010ff923a49a273e6750d
Mark <mverleg.noreply@gmail.com>
2021-05-12 23:24:44 +0200

    Update the mango.sh script

--- resource/init/mango.sh
+++ resource/init/mango.sh
@@ -4,6 +4,8 @@
 # A continuously-running daemon container is started, if it is not running.
 
 # Configuration is done through environment variables
+# - PROJECT_NAME: name of the project (no whitespace)
+# - MANGO_DOCKER_IMAGE: docker image including tag to use
 
 if [ -z "$PROJECT_NAME" ];
 then
@@ -15,26 +17,39 @@ then
     MANGO_DOCKER_IMAGE="mangocode/mango:latest"
 fi
 
-#TODO @mark: cache dir etc, from Mango itself during init? but that'd make the wrapper platform-dependent
-
-
-# Docker & image
-
-if ! command -v docker 1> /dev/null 2> /dev/null
-then
-    echo "Docker must be installed to run Mango"
-    exit 1
-fi
-
-# Mango daemon
+# Checks
 
 set -eu
 
-#TODO @mark: port might collide with other projects
+if ! command -v docker 1> /dev/null 2> /dev/null
+then
+    printf "Docker must be installed to run Mango\n" 1>&2
+    exit 1
+fi
+
+#TODO @mark: check that this is the root dir of the Mango project
+
+# Mango daemon
+
+if [ -z "$(docker ps -q -f name=mangod)" ]
+then
+    printf "Starting mango daemon container (mangod)\n"
+    docker run \
+        --name "mangod" \
+        --label started-by="$PROJECT_NAME" \
+        -d --rm \
+        -p 47558:47558 \
+        "$MANGO_DOCKER_IMAGE" \
+            -- run-as-daemon \
+            --hostname 127.0.0.1 --port 47558
+fi
+
+# Mango cli
+
 docker run \
-    --name "mangod-$PROJECT_NAME" \
-    --rm \
-    --entrypoint /mangod \
-    -p 47558:47558 \
+    --name "mango-$PROJECT_NAME" \
+    -it --rm \
+    --mount type=bind,source="$(pwd)",target=/code \
     "$MANGO_DOCKER_IMAGE" \
-        --hostname 127.0.0.1 --port 47558
+        -- "$@"
+
