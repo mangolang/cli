38d64570cfae860bc5c8d0a941d2d713e4a99b50
Mark Verleg <mverleg.noreply@gmail.com>
2021-07-07 22:44:30 +0200

    Some example changes

--- src/cli/options/init.rs
+++ src/cli/options/init.rs
@@ -5,8 +5,7 @@ pub struct InitCmd {
     #[structopt(
         short = "n",
         long = "name",
-        help = "Name of the project to initialize. Defaults to directory name."
+        help = "Name of the project to initialize. If omitted, you will be asked interactively."
     )]
     pub name: Option<String>,
-
 }
